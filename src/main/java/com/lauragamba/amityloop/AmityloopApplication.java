1fcbd80458b4b7c30399fb954b1cd1478b52cc81
Laura Gamba <lm.gamba@uniandes.edu.co>
2026-04-21 20:00:22 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/lauragamba/amityloop/AmityloopApplication.java
@@ -0,0 +1,13 @@
+package com.lauragamba.amityloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmityloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmityloopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lauragamba/amityloop/AmityloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauragamba.amityloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmityloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
